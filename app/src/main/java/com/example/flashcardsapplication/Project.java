378362f5aed6ad5366050824213273456c79b085
marbypangilinan <marbysneaker@gmail.com>
2023-03-13 00:11:45 -0700

    starting hw3

---
+++ app/src/main/java/com/example/flashcardsapplication/Project.java
@@ -0,0 +1,18 @@
+package com.example.marbysandroidproject;
+
+public class Project {
+    String projectName;
+    String description;
+    int image;
+
+    String id;
+
+
+    Project(String projectName,String description, int image, String id ){
+        this.projectName = projectName;
+        this.description = description;
+        this.image = image;
+        this.id = id;
+
+    }
+}
---
+++ app/src/main/java/com/example/flashcardsapplication/ProjectsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.flashcardsapplication;
+
+public class ProjectsAdapter {
+}
